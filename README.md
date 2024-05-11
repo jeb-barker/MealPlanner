2b58f96bae8d51797db038748eee149665eb7591
Jeb Barker <48389379+jeb-barker@users.noreply.github.com>
2024-05-11 19:21:30 -0400

    Added .gitignore, updated the README, and finished the recipe add function in the recipe manager.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # MealPlanner
 A website and interface for J, J, J, J, and B's house meal planning.
+
+# recipe_manager.py
+- This script allows for the creation and viewing of .yaml recipe files.
+- Recipes have the following fields:
+
+| Field       | Data Type        | Notes                                                                                            |
+|-------------|------------------|--------------------------------------------------------------------------------------------------|
+| Name        | String           | The name of the recipe                                                                           |
+| Source      | String           | Where the recipe came from.<br/>This is typically a link to the recipe                           |
+| Servings    | Number           | The amount of plates this will make.<br/>It could also be the number of people the recipe serves |
+| Tags        | Array of Strings | Short (1-2 word) phrases to quickly find the recipe and sort them later                          |
+| Notes       | Array of Strings | Longer notes about the recipe, ingredients, etc.                                                 |
+| Ingredients | Array of Strings | Each ingredient is represented as a string in the form  '[count,unit,ingredient name]'           |
+| Steps       | Array of Strings | Numbered list of steps to complete the recipe.                                                   |
+
+- Currently the recipe inputs are _not_ validated, and editing is not currently planned to be implemented.
+  - It is useful to note, however, that the outputted .yaml files are stored locally in recipe_sources
--- recipe_manager.py
+++ recipe_manager.py
@@ -1,12 +1,31 @@
 # This program allows the user to see/modify/add recipies in YAML format
 
-import yaml
-STATE_ENUM = {"START": 1, "RECIPE_LIST": 2, "ADD_RECIPE": 3, "ADD_INGREDIENT": 4, "NAME": 5, "SOURCE" : 6, "SERVINGS": 7, "TAGS": 8, "NOTES": 9, "ADD_STEP": 10}
+import oyaml as yaml
+import os
+import io
+
+# global enum and state
+STATE_ENUM = {"START": 1, "RECIPE_LIST": 2, "ADD_RECIPE": 3, "ADD_INGREDIENT": 4, "NAME": 5, "SOURCE" : 6, "SERVINGS": 7, "TAGS": 8, "NOTES": 9, "ADD_STEP": 10, "CHOOSE_RECIPE": 11}
 STATE = STATE_ENUM["START"]
 
 
 def print_recipe_list():
-    pass
+    directory = os.fsencode("./recipe_sources")
+
+    recipes = []
+    recipe_number = 1
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        filename = "./recipe_sources/" + filename
+        if filename.endswith(".yaml"):
+            with open(filename) as stream:
+                try:
+                    curr_recipe = yaml.safe_load(stream)
+                    recipes.append(curr_recipe)
+                    print(str(recipe_number) + ".\t" + curr_recipe["name"])
+                except yaml.YAMLError as exc:
+                    print(exc)
+    return recipes
 
 
 def print_options():
@@ -16,7 +35,7 @@ def print_options():
         print("1. View List of Recipies:")
         print("2. Add Recipe")
     if STATE == STATE_ENUM["RECIPE_LIST"]:
-        print_recipe_list()
+        print("Select one of the recipies below:")
     if STATE == STATE_ENUM["ADD_RECIPE"]:
         print("You would like to add a recipe.\nPlease enter the following fields (don't leave them blank or I'll cry).")
     if STATE == STATE_ENUM["NAME"]:
@@ -32,7 +51,90 @@ def print_options():
     if STATE == STATE_ENUM["ADD_INGREDIENT"]:
         print("Enter the count, unit, and name of the ingredient:\n(e.g. \"2,clove,garlic\")\n")
     if STATE == STATE_ENUM["ADD_STEP"]:
-        print("Enter a new step or \"-1\" to stop adding ingredients")
+        print("Enter a new step or \"-1\" to stop adding steps")
+    if STATE == STATE_ENUM["CHOOSE_RECIPE"]:
+        print("Choose a recipe from above (or -1 to go back):")
+
+
+def get_recipe_from_user():
+    global STATE
+    global STATE_ENUM
+    STATE = STATE_ENUM["NAME"]
+    print_options()
+    # get recipe name
+    user_input = input("")
+    name = user_input
+
+    STATE = STATE_ENUM["SOURCE"]
+    print_options()
+    # get recipe source
+    user_input = input("")
+    source = user_input
+
+    STATE = STATE_ENUM["SERVINGS"]
+    print_options()
+    # get recipe servings
+    user_input = input("")
+    servings = int(user_input)
+
+    STATE = STATE_ENUM["TAGS"]
+    print_options()
+    tags = []
+    # get recipe servings
+    user_input = input("")
+    while user_input != "-1":
+        tags.append(user_input)
+        print_options()
+        user_input = input("")
+
+    STATE = STATE_ENUM["NOTES"]
+    print_options()
+    notes = []
+    # get recipe servings
+    user_input = input("")
+    while user_input != "-1":
+        notes.append(user_input)
+        print_options()
+        user_input = input("")
+
+    STATE = STATE_ENUM["ADD_INGREDIENT"]
+    print_options()
+    ingredients = []
+    # get recipe servings
+    user_input = input("")
+    while user_input != "-1":
+        split_input = user_input.split(",")
+        ingredients.append("["+split_input[0]+","+split_input[1]+","+split_input[2]+"]")
+        print_options()
+        user_input = input("")
+
+    STATE = STATE_ENUM["ADD_STEP"]
+    print_options()
+    steps = []
+    # get recipe servings
+    user_input = input("")
+    while user_input != "-1":
+        steps.append(user_input)
+        print_options()
+        user_input = input("")
+
+    output_yaml(name, source, servings, tags, notes, ingredients, steps)
+
+
+def output_yaml(name, source, servings, tags, notes, ingredients, steps):
+    data = {
+        'name': name,
+        'source': source,
+        'servings': servings,
+        'tags': tags,
+        'notes': notes,
+        'ingredients': ingredients,
+        'steps': steps
+    }
+
+    # Write YAML file
+    with io.open('./recipe_sources/'+name.replace(" ", "_")+'.yaml', 'w', encoding='utf8') as outfile:
+        yaml.dump(data, outfile, default_flow_style=False, allow_unicode=True)
 
 
 def main():
@@ -53,27 +155,17 @@ def main():
         if int(user_input) == 1:
             STATE = STATE_ENUM["RECIPE_LIST"]
             print_options()
+            options = print_recipe_list()
+            user_input = int(input(""))
+            # TODO: validate:
+            print(options[user_input-1])
+
+            STATE = STATE_ENUM["CHOOSE_RECIPE"]
+            print_options()
         if int(user_input) == 2:
             STATE = STATE_ENUM["ADD_RECIPE"]
             print_options()
-
-            STATE = STATE_ENUM["NAME"]
-            print_options()
-            # get recipe name
-            user_input = input("")
-            name = user_input
-
-            STATE = STATE_ENUM["SOURCE"]
-            print_options()
-            # get recipe source
-            user_input = input("")
-            source = user_input
-
-            STATE = STATE_ENUM["SERVINGS"]
-            print_options()
-            # get recipe servings
-            user_input = input("")
-            servings = user_input
+            get_recipe_from_user()
 
 
 
